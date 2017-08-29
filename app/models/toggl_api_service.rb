cf12e6e4a2a55f7d01e4feb286370d655df10bd8
Adam Konner <linesarefuzzy@gmail.com>
2017-08-28 23:07:26 -0400

    Group and sum time entries by date and description

--- app/models/toggl_api_service.rb
+++ app/models/toggl_api_service.rb
@@ -17,9 +17,15 @@ class TogglAPIService
       workspace_ids = wid_response.select{|k| @toggl_workspace.include?(k['name'])}.map{|k| k['id']}
     end
 
-    # if user has setup workspace, use entries for those workspaces. If no workspace is setup, use all
-    get_latest_toggl_entries_api_response('time_entries').map { |entry|
-      if entry["description"] =~ /\s*#(\d+)\s*/ && !entry["stop"].nil? && !entry["stop"].empty? &&
+    entries = get_latest_toggl_entries_api_response('time_entries')
+
+    # Group and sum duration by date and description
+    grouped = entries.sort_by { |i| i['start'].to_time }.group_by { |i| [i['start'].to_date, i['description']] }.values
+    summed = grouped.map { |i| j = i[0].dup; j['duration'] = i.sum { |k| k['duration'] }; j }
+
+    summed.sort_by { |i| i['start'].to_time }.map { |entry|
+      # if user has setup workspace, use entries for those workspaces. If no workspace is setup, use all
+      if entry["description"] =~ /\s*#(\d+)\s*/ && entry["duration"] > 0 &&
       (@toggl_workspace.blank? || workspace_ids.include?(entry['wid']))
 
         TogglAPIEntry.new(entry["id"],
